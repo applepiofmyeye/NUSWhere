7dcfe05d01dad3227ac939fa1ed5bb541c3c1c70
limjunxian1 <limjunxian@live.com.sg>
2023-05-27 23:38:58 +0800

    General style for buttons:Login and Register

---
+++ components/account/CustomButton/CustomButton.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Text, StyleSheet, Pressable, View } from 'react-native'
+import { COLORS, FONT, SIZES } from "../../../constants";
+
+const CustomButton = ({onPress, text}) => {
+    return (
+    <View style={{alignItems: "center", justifyContent: "flex-end"}}>
+        <Pressable onPress={onPress} style={styles.container}>
+            <Text style={styles.text}>{text}</Text>
+        </Pressable>
+    </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: COLORS.pressedBtn,
+
+        width: 250,
+        padding: 15,
+        marginVertical : 5,
+
+        alignItems: "center",
+        borderRadius: SIZES.xLarge,
+        justifyContent: "center",
+    },
+    text: {
+        fontFamily: FONT.iSemiB,
+        fontSize: SIZES.large,
+        color: COLORS.text
+    },
+})
+
+export default CustomButton;
