095217bc3c1d0f5606d7fefa8f90a4e6fea486fd
limjunxian1 <limjunxian@live.com.sg>
2023-05-27 23:31:02 +0800

    Removed some style unused. For the style of buttons, created another file.

--- components/account/logindiv/logindiv.style.js
+++ components/account/logindiv/logindiv.style.js
@@ -2,34 +2,23 @@ import { COLORS, FONT, SIZES } from "../../../constants";
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-    loginContainer:{
+    loginContainer: {
         padding: SIZES.xLarge,
         backgroundColor: COLORS.background,
         borderRadius: SIZES.medium,
         alignItems: "center",
-        justifyContent:"space-evenly",
+        justifyContent:"space-evenly"
     },
     pageTitle: {
         fontFamily: FONT.iSemiB,
         fontSize: SIZES.xLarge,
         color: COLORS.text
     },
-    loginBtn: {
-        width: 250,
-        backgroundColor: COLORS.pressedBtn,
-        padding: 15,
-        borderRadius: SIZES.xLarge,
-        alignItems: "center",
-
-    },
-    loginBtnLabel: {
-        fontFamily: FONT.iSemiB,
-        fontSize: SIZES.large,
-        color: COLORS.text
-    },
-    loginBtnContainer: {
-        alignItems: "center",
-        justifyContent: "flex-end",
+    registerContainer: {
+        padding: 20, 
+        alignItems: "center", 
+        justifyContent: "center", 
+        flexDirection: "row"
     }
 })
 
