1f68adccf0dcf54892c0eda14f9299843bb3c61d
limjunxian1 <limjunxian@live.com.sg>
2023-05-29 01:08:00 +0800

    Created a simple home page, nothing much inside. Just to test login function

--- app/home.js
+++ app/home.js
@@ -1,1 +1,16 @@
- 
+import React from 'react'
+import { Text, StyleSheet, View } from 'react-native'
+
+const Home = () => {
+    return (
+      <View>
+        <Text> This is home page! </Text>
+      </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default Home;
