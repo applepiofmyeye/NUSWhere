1b49e66a2397ecb8d7b0baf03cfd9838006161c2
limjunxian1 <limjunxian@live.com.sg>
2023-05-29 01:17:40 +0800

    Did pop-ups for firebase related errors, updated basic errors that may occur

--- components/account/registerdiv/registerdiv.style.js
+++ components/account/registerdiv/registerdiv.style.js
@@ -23,7 +23,49 @@ const styles = StyleSheet.create({
     },
     error: {
         color: "#dc3545"
-    }
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        transition: 'opacity 250ms ease',
+        backgroundColor: 'rgba(0,0,0,0.5)',
+      },
+      modalSuccessView: {
+        justifyContent: 'center',  
+        alignItems: 'center',   
+        backgroundColor : "#32CD32",   
+        height: 200 ,  
+        width: '80%',  
+        borderRadius: 50,  
+        borderWidth: 10,  
+        borderColor: '#fff',   
+      },
+      modalText: {
+        marginBottom: 15,
+        textAlign: 'center',
+        fontFamily: FONT.pBold,
+      },
+      buttonTextStyle: {
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center',
+      },
+      modalButton: {
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2,
+      },
+      modalErrorView: {
+        justifyContent: 'center',  
+        alignItems: 'center',   
+        backgroundColor : "#EE4B2B",   
+        height: 200 ,  
+        width: '80%',  
+        borderRadius: 50,  
+        borderWidth: 10,  
+        borderColor: '#fff',
+      }
 })
 
 export default styles;
