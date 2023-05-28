90dcab08fb81af40880eb65095ade27bd7bbd005
limjunxian1 <limjunxian@live.com.sg>
2023-05-29 01:16:23 +0800

    Input Logo

--- components/welcome/Welcome.jsx
+++ components/welcome/Welcome.jsx
@@ -1,13 +1,17 @@
-import React, { View, Text, Image } from "react-native";
-import { logo } from "../../constants";
-
+import React, { View, Text, Image, /* useWindowDimensions */ } from "react-native";
+import Logo from "../../assets/images/NUSWhereLogo.png";
 import styles from "./welcome.style";
 
 export default function Welcome() {
+  /* const {height} = useWindowDimensions(); */
   return (
-    <View style={[styles.welcomeContainer, {flex: 4}]}>
+    /* {<View style={[styles.welcomeContainer, {flex: 4}]}>
         <Text style={styles.welcomeTitle}>NUSWhere?</Text>
         <Text style={styles.slogan}>here lah</Text>
+    </View>} */
+    <View style={styles.welcomeContainer}>
+      <Image source={Logo} /*styles={[styles.logo, {height: height * 0.3}]}  resizeMode="contain" *//>
+      <Text style={styles.slogan}>here lah</Text>
     </View>
   )
 }
