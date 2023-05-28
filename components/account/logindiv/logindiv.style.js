6f0df97b8c53af3caedce9fc617166006861f12c
limjunxian1 <limjunxian@live.com.sg>
2023-05-29 01:16:03 +0800

    Input styles for popups and error messages

--- components/account/logindiv/logindiv.style.js
+++ components/account/logindiv/logindiv.style.js
@@ -19,7 +19,42 @@ const styles = StyleSheet.create({
         alignItems: "center", 
         justifyContent: "center", 
         flexDirection: "row"
-    }
+    },
+    error: {
+        color: "#dc3545"
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        transition: 'opacity 250ms ease',
+        backgroundColor: 'rgba(0,0,0,0.5)',
+      },
+      modalView: {
+        justifyContent: 'center',  
+        alignItems: 'center',   
+        backgroundColor : "#EE4B2B",   
+        height: 200 ,  
+        width: '80%',  
+        borderRadius: 50,  
+        borderWidth: 10,  
+        borderColor: '#fff',   
+      },
+      modalText: {
+        marginBottom: 15,
+        textAlign: 'center',
+        fontFamily: FONT.pBold,
+      },
+      buttonTextStyle: {
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center',
+      },
+      modalButton: {
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2,
+      },
 })
 
 export default styles;
