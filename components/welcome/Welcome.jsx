77f8c9fc19458f2dc983a61c6789537e39d66309
limjunxian1 <limjunxian@live.com.sg>
2023-06-17 17:55:22 +0800

    Merge

--- components/welcome/Welcome.jsx
+++ components/welcome/Welcome.jsx
@@ -5,21 +5,9 @@ import styles from "./welcome.style";
 export default function Welcome() {
   /* const {height} = useWindowDimensions(); */
   return (
-<<<<<<< HEAD
-    /* {<View style={[styles.welcomeContainer, {flex: 4}]}>
-        <Text style={styles.welcomeTitle}>NUSWhere?</Text>
-        <Text style={styles.slogan}>here lah</Text>
-    </View>} */
-    <View style={styles.welcomeContainer}>
-      <Image source={Logo} /*styles={[styles.logo, {height: height * 0.3}]}  resizeMode="contain" *//>
-      <Text style={styles.slogan}>here lah</Text>
-    </View>
-=======
     <View style={styles.welcomeContainer}>
     <Image source={Logo} /*styles={[styles.logo, {height: height * 0.3}]}  resizeMode="contain" *//>
-    <Text style={styles.welcomeTitle}>NUSWhere?</Text>
     <Text style={styles.slogan}>here lah</Text>
   </View>
->>>>>>> f42e8917fb612e2f44c41a80ba7dab7e86924ee2
   )
 }
