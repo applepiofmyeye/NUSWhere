df62065dc54c7e6d4b108c2efb70d72ac6f892ad
limjunxian1 <limjunxian@live.com.sg>
2023-06-24 19:01:56 +0800

    Trail and error to improve autocomplete UI

--- components/Autocomplete.jsx
+++ components/Autocomplete.jsx
@@ -118,3 +118,143 @@ const Autocomplete = ({
 };
 
 export default Autocomplete;
+
+// Trying out flatList implementation to scroll through options provided but not working.
+
+/*
+import { Alert, Keyboard, View, Text, FlatList} from "react-native";
+import { Menu, TextInput } from "react-native-paper";
+import React, { useState } from "react";
+import { COLORS } from "../constants/theme";
+
+// import hashmap of room codes-coords
+import { busStopCoords, roomCodeCoords } from "../data/venues";
+import { TouchableOpacity } from "react-native-web";
+
+
+
+
+
+const Autocomplete = ({
+    value: origValue,
+    label,
+    data,
+    containerStyle,
+    onChange: origOnChange,
+    icon = 'bike',
+    style = {},
+    menuStyle = {},
+    right = () => {},
+    left = () => {},
+    usage,
+    menuThenGoTo,
+    selectedMarker,
+    onSelectMarker,
+    isDestination
+
+  }) => {
+    const [value, setValue] = useState(origValue);
+    const [menuVisible, setMenuVisible] = useState(false);
+    const [filteredData, setFilteredData] = useState([]);
+  
+    const filterData = (text) => {
+      return data.filter(
+        (val) => val.name?.toLowerCase()?.indexOf(text?.toLowerCase()) > -1 // changed from val to val.name
+      );
+    };
+    
+    const renderItem = ({ item }) => {
+      //console.log(item);
+      return (
+      <View>
+      <TouchableOpacity 
+        style={[{ width: '100%' }, menuStyle]}
+        onPress={() => {
+          // to allow for other uses: eg searching bus stops
+          if (usage === 'mappage') {
+            let location;
+            if (roomCodeCoords.get(item.name) != null) {
+              location = roomCodeCoords.get(item.name)[2];
+              const markerLocation = {latitude: location.y, longitude: location.x}; 
+              console.log(markerLocation)
+              onSelectMarker(markerLocation, isDestination, item);
+            } else if (busStopCoords.get(item.name) != null) {
+              location = busStopCoords.get(item.name); 
+              const markerLocation = {latitude: location.latitude, longitude: location.longitude}; 
+              console.log(markerLocation)
+              onSelectMarker(markerLocation, isDestination, item);
+            } else {
+              return Alert.alert("Location Unavailable", "Sorry! We currently don't have enough data for this venue.", [{
+                text: "OK",
+                onPress: () => {},
+                style: 'cancel'                      
+              }])
+            }
+
+            setValue(item.name);
+            //setMenuVisible(false);
+            Keyboard.dismiss();
+            
+          }
+        }}>
+        <Text>{value}</Text>
+      </TouchableOpacity>
+      </View>
+    );}
+
+
+    return (
+      <View style={[containerStyle]}>
+        <TextInput
+          onFocus={() => {
+            if (value.length === 0) {
+              setMenuVisible(true);
+            }
+          }}
+          
+          // onBlur={() => setMenuVisible(false)}
+          placeholder={label}
+          right={right}
+          left={left}
+          style={style}
+          onChangeText={(text) => {
+            origOnChange(text); // changed, intial: origOnChange(text)
+            if (text && text.length > 0) { // remove text &&
+              setValue(text); // added
+              setFilteredData(filterData(text));
+              setMenuVisible(true); // added
+            } else if (text && text.length === 0) {
+              setFilteredData(data);
+              setMenuVisible(false); // added
+            }
+            //setMenuVisible(true);
+            //setValue(text);
+          }}
+          value={value}
+        />
+        {menuVisible? (
+          <View
+            style={{
+              flex: 1,
+              backgroundColor: COLORS.background,
+              height: 300, // added
+              width: '90%', // added 
+              borderWidth: 1,
+              flexDirection: 'column',
+              borderColor: COLORS.text,
+            }}
+          >
+            
+            <FlatList
+              data={filteredData}
+              renderItem={renderItem}
+              keyExtractor={(item) => item.name}
+            />
+          </View>
+        ) : null }
+      </View>
+    );
+  };
+  
+  export default Autocomplete;
+*/
