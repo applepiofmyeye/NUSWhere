56e3c6c56f795d3b2aadd49ae109330726106fa1
limjunxian1 <limjunxian@live.com.sg>
2023-06-24 18:39:26 +0800

    Create logout button, trying to fix why FRList crashes and doesn't work

--- app/screens/profile.js
+++ app/screens/profile.js
@@ -7,7 +7,7 @@ add backend data -- users' fav routes
 add nav bar
 */
 
-import React, { SafeAreaView, View, ScrollView } from "react-native";
+import React, { TouchableOpacity, View, Text, Alert, Pressable } from "react-native";
 import { Stack, useRouter } from "expo-router";
 import { COLORS, FONT } from "../../constants/theme";
 import {
@@ -15,11 +15,26 @@ import {
     FavouriteRoutesDiv
 } from "../../components";
 import { auth } from "../firebase";
-
+import { AuthStore } from "../../store";
+import { signOut } from "firebase/auth/react-native";
 
 export default function Profile() {
-    const user = auth.currentUser;
-    const name = user.displayName;
+    //const user = auth.currentUser;
+    //const name = user.displayName;
+
+    const router = useRouter();
+    const appSignOut = async () => {
+        try {
+          await signOut(auth);
+          AuthStore.update((store) => {
+            store.user = null;
+            store.isLoggedIn = false;
+          });
+          return { user: null };
+        } catch (e) {
+          return { error: e };
+        }
+      };
     
     return (
         <View style={{ flex: 1, backgroundColor: COLORS.background,}}>
@@ -29,9 +44,23 @@ export default function Profile() {
                     }}
             />
             
-            
             <View style={[{flex: 1, backgroundColor: COLORS.background}]}>
-                <Greeting mainText="Welcome, " subText={name}/>
+
+                <TouchableOpacity 
+                    onPress={async () => {
+                        const resp = await appSignOut();
+                        if (!resp?.error) {
+                        router.replace("../auth/login");
+                        } else {
+                        console.log(resp.error);
+                        Alert.alert("Logout Error", resp.error?.message);
+                        }}} 
+                    style={{ paddingTop: 35, alignItems: "flex-end", marginRight: 10 }}>
+                    <Text>Logout</Text>
+                </TouchableOpacity>
+
+                <Greeting mainText="Welcome, " subText={AuthStore.getRawState().user?.displayName}/>
+                <FavouriteRoutesDiv/>
 
             </View>
         </View>
