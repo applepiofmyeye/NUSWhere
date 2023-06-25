c36c70bfe55949cbc6c2452a4cf288ec2938c9ba
limjunxian1 <limjunxian@live.com.sg>
2023-06-25 17:06:05 +0800

    Edited flex

--- components/profile/greeting/Greeting.jsx
+++ components/profile/greeting/Greeting.jsx
@@ -4,7 +4,7 @@ import styles from "./greeting.style";
 export default function Greeting({mainText, subText}) {
     const nameComponent = <Text style={styles.name}>{subText}</Text>
   return (
-    <View style={[styles.greetingContainer, {flex: 2}]}>
+    <View style={[styles.greetingContainer, {flex: 1}]}>
         <Text style={styles.greeting}>{mainText}{nameComponent}</Text>
     </View>
   )
