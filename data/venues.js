0983ec01671d141f81c888e37aa8f1166cd2b743
limjunxian1 <limjunxian@live.com.sg>
2023-06-25 17:07:21 +0800

    Revert changes

--- data/venues.js
+++ data/venues.js
@@ -10,7 +10,8 @@ const roomCodeCoords = new Map();
       const floor = room.floor;
       const location = room.location;
       roomCodeCoords.set(roomCode, [roomName, floor, location]);
-      roomCodes.push(roomCode); // roomCodes.push({id: roomName, name: roomCode}); change for autocomplete to add a key id.
+      roomCodes.push(roomCode); 
+      //roomCodes.push({id: roomName, name: roomCode}); // change for autocomplete to add a key id.
     }
 
 const busStops = [];
@@ -24,7 +25,8 @@ const busStopCoords = new Map();
         longitude: busStop.location[1]
       };
       busStopCoords.set(busStopName, busStopLocation);
-      busStops.push(busStopName); // busStops.push({id: busStopName, name: busStopName}); change for autocomplete to add a key id.
+      busStops.push(busStopName); 
+      // busStops.push({id: busStopName, name: busStopName}); // change for autocomplete to add a key id.
     }
     //console.log(busStopCoords)
 
