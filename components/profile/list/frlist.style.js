e4d2f0fc7fb4f97c1deb9b9a5268de643cdd8e81
limjunxian1 <limjunxian@live.com.sg>
2023-06-25 17:06:44 +0800

    Edited margin

--- components/profile/list/frlist.style.js
+++ components/profile/list/frlist.style.js
@@ -22,7 +22,7 @@ const styles = StyleSheet.create({
         fontFamily: FONT.pSemiB,
         fontSize: SIZES.xLarge + 1,
         color: COLORS.text,
-        margin: 10,
+        marginHorizontal: 10,
     },
 
     titleContainer: {
