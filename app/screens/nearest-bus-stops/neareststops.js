bafe27abc0625214b72f96a5f460297a5e69bef2
limjunxian1 <limjunxian@live.com.sg>
2023-07-21 22:32:13 +0800

    Updated some errors that users may face and how app should respond, tidy up code

--- app/screens/nearest-bus-stops/neareststops.js
+++ app/screens/nearest-bus-stops/neareststops.js
@@ -1,40 +1,16 @@
-/*
-Map page where users will key in their go-tos.
-
-TODO:
-make this page.
-*/
-
-import React, { SafeAreaView, View, ScrollView, Text, StyleSheet, Pressable} from "react-native";
-import { Stack, useRouter } from "expo-router";
-
+import React, { SafeAreaView, View, Text, StyleSheet, Pressable, Alert} from "react-native";
+import { Stack } from "expo-router";
 import { COLORS, FONT, SIZES } from "../../../constants";
 import { FlatList } from "react-native-gesture-handler";
-
-import axios from "axios";
 import * as Location from 'expo-location';
-import { useState, useEffect } from "react";
-import { GOOGLE_API } from "../../../keys";
+import { useState, useEffect} from "react";
 import * as geolib from 'geolib';
 import { CustomButton } from "../../../components";
-import { v4 as uuidv4 } from 'uuid';
-
-
-
-
-
-
 
 
 export default function NearestStops() {
     console.log("in Neareststops");
-    const [lat1, setLat1] = useState(null)
-    const [long1, setLong1] = useState(null)
-    // const [busStopOrderedData, setBusStopOrderedData] = useState([])
-    const [errorMsg, setErrorMsg] = useState("");
     const [loading, setLoading] = useState(true);
-    console.log("loading: ", loading);
-
 
     const busStopJson = require("../../../data/bus-stops.json")
     const busStopData = [];
@@ -59,79 +35,68 @@ export default function NearestStops() {
         }
     })
 
-    const [showBuses, setShowBuses] = useState(new Array(busStopCoords.length).map(x => false))
-
-
-    
+    const [showBuses, setShowBuses] = useState(new Array(busStopCoords.length).map(x => false))    
     const [busStopOrderedData, setBusStopOrderedData] = useState([])
-    
 
+    async function getLocationAsyncWithTimeout() {
+        const locationPromise = Location.getCurrentPositionAsync({ enableHighAccuracy: true });
+      
+        try {
+            const location = await Promise.race([
+                locationPromise,
+                new Promise((resolve, reject) => setTimeout(() => reject(new Error('Unable to retrieve location. Please refresh.')), 10000))
+            ]);
+            console.log('Location:', location);
+            return location;
+        } catch (error) {
+            // Handle the error if the location couldn't be obtained within the specified timeout
+            Alert.alert("Request timeout", error.message);
+            setLoading(false);
+        }
+      }
     
-    useEffect(() => {
-        const findUserLocation = async () => {
-
-            
-            console.log("Accessing your location..");
+    const findUserLocation = async () => {
+	
+        try {
             let {status} = await Location.requestForegroundPermissionsAsync();
 
             if (status !== 'granted') {
-                setErrorMsg('Permission to access location was denied.')
-            }
-            console.log("Accessed!");
-
-            console.log("Getting current position..");
-            let location = await Location.getCurrentPositionAsync({enableHighAccuracy: true}); 
-
-            console.log("retrieved!");
-
-            setLat1(location.coords.latitude);
-            setLong1(location.coords.longitude)
-            console.log("location", location);
-
-            console.log("latitude", location.coords.latitude);
-            console.log("longitude", location.coords.longitude);
-
-            // console.log(findDistanceBetween2());
-            
-
-            
-            if (lat1 !== null) {
-                try {
+                Alert.alert(
+                    'Location Access',
+                    'Permission to access location was denied. The bus stops displayed will not be accurately near to your location. ' +
+                    'Allow location access in your phone settings for better functionality.'
+                );
+                setBusStopOrderedData(busStopData);
+                setLoading(false);
+            } else {
+                getLocationAsyncWithTimeout().then(loc => {
                     const orderedData = geolib.orderByDistance(
-                    { latitude: lat1, longitude: long1 },
-                  busStopCoords
-                ).map((x) => {
+                        { latitude: loc.coords.latitude, longitude: loc.coords.longitude },
+                        busStopCoords
+                    ).map((x) => {
                     return busStopData.find(
-                        (busStop) =>
-                        x.latitude === busStop.latitude && x.longitude === busStop.longitude
-                    );
-                });
-          
-                setBusStopOrderedData(orderedData);
-                } catch (error) {
-                    console.log(error);
-                    console.log("lat1:", lat1);
-                    console.log("long1: ", long1);
-                }
-               
-            }
-
-
-            console.log(busStopOrderedData);
-            
-            if (busStopOrderedData[0]) {
-                console.log("busStop ordered data is ", busStopOrderedData);
-                setLoading(false)
-            }
-            
+                            (busStop) =>
+                            x.latitude === busStop.latitude && x.longitude === busStop.longitude
+                        );
+                    });
+                    setBusStopOrderedData(orderedData);
+                    setLoading(false);
+                }).catch(error => {console.log("Error:", error);});
+            } 
+        } catch (error) {
+            console.log("Error:", error);
         }
-
         
+    }
 
-        if (loading) {findUserLocation();}
+    const onRefresh = () => {
+        setLoading(true);
+        findUserLocation();
+    }
 
-
-    },[lat1, long1])
+    useEffect(() => {
+        findUserLocation();
+    },[]);
 
     const getColorForRoute = (route) => {
         return route == "A1" 
@@ -151,8 +116,6 @@ export default function NearestStops() {
                : COLORS.busL
     }
 
-
-
     const renderItem = (item) => {
         return (
         <Pressable 
@@ -165,10 +128,10 @@ export default function NearestStops() {
                 updatedState[item.item.id] = !previousState
                 return updatedState;
             })}>
+
             <Text style={styles.nameStyle}>{item.item.name}</Text>
-            { showBuses[item.item.id] && 
-                
-                    
+
+            { showBuses[item.item.id] &&                     
                 <View style={styles.busRoute}>
                     {item.item.routes.map((route, index) => (
                         <View
@@ -187,24 +150,13 @@ export default function NearestStops() {
                                     }}>
                                 <Text style={{color: "white"}}>NA</Text>
                             </View>
-                        </View>
-                        
+                        </View>                        
                     ))}
-                </View>
-            
-            }            
+                </View>            
+            }  
 
         </Pressable>)
     }
-
-    const Buses = () => {
-        return (
-            <View >
-                
-            </View>
-        )
-    }
-
     
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.background }}>
@@ -218,32 +170,29 @@ export default function NearestStops() {
                     }
                 }}
             />
+
             {busStopOrderedData &&  
-            <FlatList
-                data={busStopOrderedData}
-                renderItem={renderItem}
-                keyExtractor={(item, index) => `${item.id}-${index}`}
+                <FlatList
+                    data={busStopOrderedData}
+                    renderItem={renderItem}
+                    keyExtractor={(item, index) => `${item.id}-${index}`}
             />}
 
             {loading && <Text>Loading..</Text>}
 
             {busStopOrderedData.length === 0 && !loading &&
-            <View>
-                <Text>No data available.</Text>
-                <CustomButton
-                    text={"refresh"}
-                    onPress={() => setLoading(true)}></CustomButton>
+                <View>
+                    <Text>No data available.</Text>
+                    <CustomButton
+                        text={"REFRESH"}
+                        onPress={() => onRefresh()}></CustomButton>
 
-            </View>
+                </View>
             }
-
-            
-            
 
         </SafeAreaView>
     )
 }
-
 
 const styles = StyleSheet.create({
     itemContainer: {
@@ -275,7 +224,5 @@ const styles = StyleSheet.create({
         color: COLORS.text,
         fontSize: SIZES.medium + 1
     }
-
-
 })
 
